c92896482d23a2c72ac9aca6bbf9c812e9d84483
Chris <BOBitisme@gmail.com>
2023-07-07 22:39:23 +1200

    Updated connection php files

--- docker/php/src/includes/public_connection.php
+++ docker/php/src/includes/public_connection.php
@@ -1,7 +1,6 @@
 <?php
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    // $dbc = mysqli_connect("db", "root", "example", "fundraiser", 3306); // change to public
-    $dbc = mysqli_connect("localhost", "root", "", "fundraiser", 3306);
+    $dbc = mysqli_connect("db", "public", "change-me", "fundraiser", 3306); // change to public
     if ($dbc == null) {
         echo "<h1>Sorry, you could not connect to the database</h1>";
         exit();
